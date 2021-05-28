3f00cdbfc2c30a1a8e02f4a4b20cf0b93ab2d9ff
haku1217 <haruki12.17birthday@gmail.com>
2021-05-28 10:13:27 +0900

    first commit

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World');
+        return 0;
+    };
+    return Startup;
+}());
+var aa = "dfasfa";
+console.log(aa);
+Startup.main();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World');
+        return 0;
+    }
+}
+
+const aa = "dfasfa"
+
+console.log(aa)
+Startup.main();
